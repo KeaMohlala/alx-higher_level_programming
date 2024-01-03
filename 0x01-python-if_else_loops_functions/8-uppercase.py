4c0b24aba9b3166d138bfeec095b78d62cda62cb
KeaMohlala <keamogetsemhll@gmail.com>
2024-01-03 15:34:37 -0800

    fix prototypes

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def uppercase(string):
-    for index in string:
+def uppercase(str):
+    for index in str:
         if ord(index) >= 97 and ord(index) <= 122:
             index = chr(ord(index) - 32)
         print("{}".format(index), end="")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def print_last_digit(digit):
-    if digit < 0:
-        ldigit = digit % -(10)
+def print_last_digit(number):
+    if number < 0:
+        ldigit = number % -(10)
         print(-(ldigit), end="")
     else:
-        ldigit = digit % 10
+        ldigit = number % 10
         print(ldigit, end="")
     return abs(ldigit)
