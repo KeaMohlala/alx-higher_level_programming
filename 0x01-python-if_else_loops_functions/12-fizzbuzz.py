6614821b01e4e2d4b2d80eea9a78508b1479ac47
KeaMohlala <keamogetsemhll@gmail.com>
2024-01-03 16:01:36 -0800

    task12 fix pycodestyle error

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
         if n % 3 == 0 and n % 5 == 0:
             print("FizzBuzz ", end="")
         elif n % 3 == 0:
-            print("Fizz ",end="")
+            print("Fizz ", end="")
         elif n % 5 == 0:
             print("Buzz ", end="")
         else:
