162bf285ada561ee435ba73892828ecce5bc0de9
KeaMohlala <keamogetsemhll@gmail.com>
2024-01-04 06:28:26 -0800

    import each function

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-import calculator_1
+from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
+sum_1 = add(a, b)
+diff = sub(a, b)
+product = mul(a, b)
+quoitent = div(a, b)
+
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
-    print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
-    print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+    print("{} + {} = {}".format(a, b, sum_1))
+    print("{} - {} = {}".format(a, b, diff))
+    print("{} * {} = {}".format(a, b, product))
+    print("{} / {} = {}".format(a, b, quoitent))
