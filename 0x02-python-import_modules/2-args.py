50c0a6f738cac929e98f6f6f22bd537ac9793eb5
KeaMohlala <keamogetsemhll@gmail.com>
2024-01-04 07:57:56 -0800

    code not executable when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import sys
-arg_count = (len(sys.argv)) - 1
-if arg_count == 1:
-    print("{} argument:".format(arg_count))
-elif arg_count == 0:
-    print("{} arguments.".format(arg_count))
-else:
-    print("{} arguments:".format(arg_count))
-for i in range(arg_count):
-    print("{}: {}".format(i + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+    arg_count = (len(sys.argv)) - 1
+    if arg_count == 1:
+        print("{} argument:".format(arg_count))
+    elif arg_count == 0:
+        print("{} arguments.".format(arg_count))
+    else:
+        print("{} arguments:".format(arg_count))
+    for i in range(arg_count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
