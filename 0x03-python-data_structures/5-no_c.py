253c939c5b50892a21d7d26b7f955d1b2a1762b8
KeaMohlala <keamogetsemhll@gmail.com>
2024-01-06 07:01:04 -0800

    task5 use list comprehension

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string)):
-        if my_string[i] == 'c' or my_string[i] == 'C':
-            new_string = my_string[:i] + my_string[i+1:]
-            return new_string
+    return "".join(char for char in my_string if char not in ['c', 'C'])
