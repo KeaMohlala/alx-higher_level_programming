ef38ef000beb9fbfd0e3f95a90594843f2faf9a7
KeaMohlala <keamogetsemhll@gmail.com>
2024-01-06 11:57:26 -0800

    task9 accomodate list with only negative integers

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    _max = 0
     if len(my_list) == 0:
         return None
-    else:
-        for i in range(len(my_list)):
-            is_max = my_list[i]
-            if is_max >= _max:
-                _max = is_max
-        return _max
+    _max = my_list[0]
+    for i in range(len(my_list)):
+        is_max = my_list[i]
+        if is_max >= _max:
+            _max = is_max
+    return _max
