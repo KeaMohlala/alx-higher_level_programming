f74072fa3bc104681d6108f7197d8c7ae99da33c
KeaMohlala <keamogetsemhll@gmail.com>
2024-01-11 13:41:19 -0800

    task10 edge case empty dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    if not a_dictionary.keys():
+        return None
     max_score = next(iter(a_dictionary))
     for key in a_dictionary:
         if a_dictionary[key] >= a_dictionary[max_score]:
