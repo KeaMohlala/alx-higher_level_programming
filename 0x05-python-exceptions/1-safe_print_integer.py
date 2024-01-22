8f9016626baff3adc1b069583020e1eb967d6338
KeaMohlala <keamogetsemhll@gmail.com>
2024-01-22 05:01:13 -0800

    check if value is string with only digits

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    if isinstance(value, str):
+        if not value.replace(" ", "").isdigit():
+            return False
     try:
-        print("{:d}".format(value))
+        print("{:d}".format(int(value)))
     except ValueError:
         return False
     return True
