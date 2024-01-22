1fde54bb8a28442729f4dcab50d3ef6e8a4a3b73
KeaMohlala <keamogetsemhll@gmail.com>
2024-01-22 05:04:53 -0800

    Revert "check if value is string with only digits"
    This reverts commit 8f9016626baff3adc1b069583020e1eb967d6338.

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if isinstance(value, str):
-        if not value.replace(" ", "").isdigit():
-            return False
     try:
-        print("{:d}".format(int(value)))
+        print("{:d}".format(value))
     except ValueError:
         return False
     return True
