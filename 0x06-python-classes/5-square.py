7fe361de759dca5a05a94e6adcf1248e4a4ef3d2
KeaMohlala <keamogetsemhll@gmail.com>
2024-01-24 03:32:49 -0800

    task5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+Class that defines a square
+"""
+
+
+class Square:
+    """
+    Documentation for square class
+    """
+    def __init__(self, size=0):
+        """
+        Documentation for initilization of instance
+        it instantiates a Square object with an optional size
+
+        Args:
+          size: size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        getter property to retrieve size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        setter to set the size attribute
+
+        Args:
+          value: size of the square
+
+        Raises:
+          TypeError: if size is not an integer
+          ValueError: If size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        method that returns are of square based on size
+
+        Returns:
+          int: The area of the square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        prints in stdout the square with the character #
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size - 1):
+                print('#' * self.__size)
+            print('#' * self.__size)
