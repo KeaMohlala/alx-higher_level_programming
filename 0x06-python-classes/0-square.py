949bb46ba1a2e0ac9dababcd78e297b96ab17f2d
KeaMohlala <keamogetsemhll@gmail.com>
2024-01-23 16:29:00 -0800

    add doc strings

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
 class Square:
-    pass
+    """
+    This is the documentation for the Square class.
+
+    Attributes:
+     None.
+
+    Methods:
+      __init__(): Initializes a new object of the class.
+    """
+
+    def __init__(self):
+        """
+        Initializes object right after it's created.
+
+        Parameters:
+          None.
+
+        Returns:
+          None.
+        """
+        pass
