24bbccf4fb0ddc82132f998c08e37afbf3329196
KeaMohlala <keamogetsemhll@gmail.com>
2024-01-23 17:00:25 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Class that defines a square
+"""
+
+
+class Square:
+    """
+    Documentation for square class
+    """
+    def __init__(self, size):
+        """
+        Documentation for initilization of instance
+
+        Args:
+          size: size of the square
+        """
+        self.__size = size
