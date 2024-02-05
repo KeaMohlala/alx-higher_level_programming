d9d13124dd2ab893c014264b82905167e3fa5688
KeaMohlala <keamogetsemhll@gmail.com>
2024-02-05 07:45:25 -0800

    task 1 remove list class

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,49 +1,17 @@
 #!/usr/bin/python3
 """
-Module to illustrate inheritance
+Module with a class inherited
+from a 'list' class
 """
-
-
-class list:
-    """
-    Parent class
-    """
-    def __init__(self):
-        """
-        instantiates instances of the class with two
-        empty lists
-        """
-        self.list = []
-        self.new_list = []
-
-    def append(self, item):
-        """
-        appends new item to empty list
-
-        Args: item
-
-        Return: appended list
-        """
-        self.list.append(item)
-
-    def print_sorted(self):
-        """
-        copies list to a new lists and sorts it
-        into ascending order. new list is printed
-        """
-        self.new_list.extend(self.list)
-        self.new_list.sort()
-        print(self.new_list)
-
-    def __str__(self):
-        """
-        prints object as a string
-        """
-        return str(self.list)
 
 
 class MyList(list):
     """
-    subclass
+    subclass that inherits the list
     """
-    pass
+
+    def print_sorted(self):
+        """
+        prints list in ascending order
+        """
+        print(sorted(self))
