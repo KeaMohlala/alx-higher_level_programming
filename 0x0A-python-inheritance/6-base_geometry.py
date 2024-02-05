045058b3ceb4c18a3bb09bbe104e28d0ac25c32a
KeaMohlala <keamogetsemhll@gmail.com>
2024-02-05 11:51:08 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module for Geometry class
+"""
+
+
+class BaseGeometry:
+    """
+    creation of class 'BaseGeometry'
+    """
+    def area(self):
+        """
+        method to implement the area
+        """
+        raise Exception("area() is not implemented")
