19deea37100699d696caa84238edff60a083a5c4
KeaMohlala <keamogetsemhll@gmail.com>
2024-02-07 08:25:15 -0800

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A module with a function that creates an object from a JSON file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    function that creates an object from a JSON file
+
+    Args:
+     filename: name of JSON file
+    """
+    with open(filename, mode="r", encoding="utf-8") as f:
+        return json.load(f)
