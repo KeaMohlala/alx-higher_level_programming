64d0f5a4e06353765ec7b3ed98b1f6a7d9a18f00
KeaMohlala <keamogetsemhll@gmail.com>
2024-02-10 11:54:00 -0800

    create square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,33 @@
+#!/usr/bin/python3
+"""
+module for square class with Rectangle as
+the superclass
+"""
+from models.rectangle import Rectangle
 
+
+class Square(Rectangle):
+    """
+    describes Square class
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        class constructor calls superclass
+        all attributes are from the 'Rectangle' class
+
+        Args:
+        size: size of the square
+        x: x position of the square
+        y: y position of the square
+        id: id of created instance
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        returns string representation of the square
+        """
+        return (
+            f"[{self.__class__.__name__}] ({self.id}) "
+            f"{self.x}/{self.y} - {self.width}"
+        )
