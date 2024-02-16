7086a1324fb19a015044e03aad908417ba6e8589
KeaMohlala <keamogetsemhll@gmail.com>
2024-02-16 13:49:02 -0800

    tests for json string methods

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,6 +4,7 @@ Unittest for Base superclass
 """
 
 import unittest
+import json
 from models.base import Base
 from models.rectangle import Rectangle
 
@@ -29,3 +30,30 @@ class TestID(unittest.TestCase):
         self.assertEqual(self.b4.id, 3)
         self.assertEqual(self.b5.id, 6)
         self.assertEqual(self.b6.id, 4)
+
+
+class test_jsonstring(unittest.TestCase):
+    """
+    test cases for the to_json_string static method
+    """
+    def test_json_string(self):
+        """
+        tests for 'to_json_string' method
+        """
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+        self.assertEqual(
+                Base.to_json_string([{'id': 12}]),
+                json.dumps([{'id': 12}])
+        )
+
+    def test_fromjson_string(self):
+        """
+        tests for 'from_json_string' method
+        """
+        self.assertEqual(Base.from_json_string(None), [])
+        self.assertEqual(Base.from_json_string("[]"), [])
+        self.assertEqual(
+                Base.from_json_string('[{"id": 89}]'),
+                json.loads('[{"id": 89}]')
+        )
