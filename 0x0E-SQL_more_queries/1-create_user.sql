537d9bb80f2e1e02988469f8ade4a89cd3815ca0
KeaMohlala <keamogetsemhll@gmail.com>
2024-03-14 15:16:02 -0700

    task1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- create user if it doesn't exists
+-- grant all privileges to the user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES
