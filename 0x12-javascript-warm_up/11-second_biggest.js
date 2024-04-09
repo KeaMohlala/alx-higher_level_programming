14f378dee48ccfb16de8a5492d4bf9feb4130708
KeaMohlala <keamogetsemhll@gmail.com>
2024-04-09 05:00:39 -0700

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+function SecondLargest (arr) {
+  let firstMax = -Infinity;
+  let secondMax = -Infinity;
+
+  for (const num of arr) {
+    if (num > firstMax) {
+      secondMax = firstMax;
+      firstMax = num;
+    } else if (num > secondMax && num !== firstMax) {
+      secondMax = num;
+    }
+  }
+  if (secondMax === -Infinity) {
+    return firstMax;
+  }
+  return secondMax;
+}
+
+const { argv } = process;
+const numArgs = argv.length;
+
+if (numArgs <= 3) {
+  console.log('0');
+} else {
+  console.log(SecondLargest(argv));
+}
