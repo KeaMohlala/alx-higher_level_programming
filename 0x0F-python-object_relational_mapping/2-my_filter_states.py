86d79df9d29db5d723d8e2958975790f08967766
KeaMohlala <keamogetsemhll@gmail.com>
2024-04-13 11:25:21 -0700

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+module of a script that list all states from the database
+where the name matches the provided argument
+"""
+import MySQLdb
+import sys
+
+
+def main(user, password, db_name, state_name):
+    """
+    function that carries out the purpose of the script i.e
+    listing all states from database 'hbtn_0e_0_usa'
+    where the name matches the argument
+
+    Args: username, password, database name and state name to search
+    """
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=user,
+        passwd=password,
+        db=db_name,
+        port=3306
+    )
+
+    cursor = conn.cursor()
+
+    # prepare the SQL query with the user input
+    query = """
+        SELECT * FROM states
+        WHERE name LIKE BINARY '{}'
+        ORDER BY id ASC;
+    """.format(state_name)
+
+    # execute the query with the state name as a parameter
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    user, password, db_name, state_name = sys.argv[1:]
+    main(user, password, db_name, state_name)
